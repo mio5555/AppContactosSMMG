b1a781b1e7101e5e3ce724378dcf005839a5f9e0
Samuel <samuelmdj11@gmail.com>
2025-12-21 20:24:02 +0100

    Primer commit

---
+++ app/src/main/java/com/dam2at12/agendacontactoproyecto/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.dam2at12.agendacontactoproyecto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dam2at12.agendacontactoproyecto.ui.theme.AgendaContactoProyectoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            AgendaContactoProyectoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Text("holaaa prueba commit")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AgendaContactoProyectoTheme {
+        Greeting("Android")
+    }
+}
