232a117393c75ea2bbe68f7a62107dc4c3c2c646
Samuel <samuelmdj11@gmail.com>
2026-02-09 02:43:11 +0100

    FINAL - Correción de errores

--- app/src/main/java/com/dam2at12/agendacontactoproyecto/data/Screens.kt
+++
@@ -1,7 +0,0 @@
-package com.dam2at12.agendacontactoproyecto.data
-
-sealed class Screens (val name: String) {
-    object LoginScreen: Screens("login")
-    object HomeScreen: Screens("home")
-    object ContactDetailScreen: Screens("contact")
-}
--- app/src/main/java/com/dam2at12/agendacontactoproyecto/data/SelectedContact.kt
+++
@@ -1,8 +0,0 @@
-package com.dam2at12.agendacontactoproyecto.data
-
-/*Creamos una variable compartida para guardar el contacto al que queramos ir.
-El valor tiene que ser nullable, ya que no hay valor por defecto al iniciar la app.
- */
-object SelectedContact {
-    var contact: Contact? = null
-}
